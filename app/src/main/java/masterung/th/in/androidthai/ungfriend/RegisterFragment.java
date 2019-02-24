ee9808f3f8e99305cea2a288b48bb7bcbc50f271
ploychanok <suthamasying@gmail.com>
2019-02-24 14:07:01 +0700

    FTP OK

--- app/src/main/java/masterung/th/in/androidthai/ungfriend/RegisterFragment.java
+++ app/src/main/java/masterung/th/in/androidthai/ungfriend/RegisterFragment.java
@@ -1,16 +1,32 @@
 package masterung.th.in.androidthai.ungfriend;
 
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+
+import it.sauronsoftware.ftp4j.FTPClient;
+import it.sauronsoftware.ftp4j.FTPDataTransferListener;
 
 
 /**
@@ -18,8 +34,10 @@ import android.view.ViewGroup;
  */
 public class RegisterFragment extends Fragment {
 
+    //    Explicit
     private boolean aBoolean = true;
-
+    private ImageView imageView;
+    private Uri uri;
 
     public RegisterFragment() {
         // Required empty public constructor
@@ -32,38 +50,171 @@ public class RegisterFragment extends Fragment {
 //        Create Toolbar
         createToolbar();
 
+        //        Choose Image
+        chooseImage();
+
 
     }   // Main Method
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId()==R.id.itemUpload)
-        {checkvalue();}
-        return super.onOptionsItemSelected(item);
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+
+            uri = data.getData();
+            aBoolean = false;
+
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap, 800, 600, false);
+                imageView.setImageBitmap(bitmap1);
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        }   // if
+
+    }
+
+    private void chooseImage() {
+        imageView = getView().findViewById(R.id.imvAvata);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Please Choose App"), 1);
+
+            }
+        });
     }
 
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
-    private void checkvalue(){
-        MyAlert myAlert = new MyAlert(getActivity());
+        if (item.getItemId() == R.id.itemUpload) {
 
-
-        if (aBoolean){
-            myAlert.normalDialog("Non Choose Image", "Please Choose Avats");
+            checkValue();
 
         }
 
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void checkValue() {
+
+        MyAlert myAlert = new MyAlert(getActivity());
+
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText userEditText = getView().findViewById(R.id.edtUser);
+        EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+        String name = nameEditText.getText().toString().trim();
+        String user = userEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+
+        if (aBoolean) {
+//            Non Choose Image
+            myAlert.normalDialog("Non Choose Image", "Please Choose Avata");
+        } else if (name.isEmpty() || user.isEmpty() || password.isEmpty()) {
+//            Have Space
+            myAlert.normalDialog("Have Space", "Please Fill All Blank");
+        } else {
+
+//            upload Image To Server
+            String pathImageString = null;
+            String[] strings = new String[]{MediaStore.Images.Media.DATA};
+            Cursor cursor = getActivity().getContentResolver().query(uri, strings, null, null, null);
+            if (cursor != null) {
+
+                cursor.moveToFirst();
+                int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                pathImageString = cursor.getString(index);
+
+            } else {
+                pathImageString = uri.getPath();
+            }
+
+            Log.d("24FebV1", "path ==> " + pathImageString);
+            String nameImage = pathImageString.substring(pathImageString.lastIndexOf("/"));
+            Log.d("24FebV1", "NameImage ==> " + nameImage);
+
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            File file = new File(pathImageString);
+            FTPClient ftpClient = new FTPClient();
+           try {
+
+               //File file = new File(pathImageString);
+               //FTPClient ftpClient = new FTPClient();
+               ftpClient.connect("ftp.androidthai.in.th",21);
+               ftpClient.login("ksu@androidthai.in.th","Abc12345");
+               ftpClient.changeDirectory("ploycha");
+               ftpClient.upload(file,new uploadListener());
+           }catch (Exception e){
+               e.printStackTrace();
+               try {
+                   ftpClient.disconnect(true);
+               }catch (Exception e1)
+               {
+                   e1.printStackTrace();
+
+               }
+
+           }
+
+        }   // if
+
+    }   // checkValue
+
+    public class uploadListener implements FTPDataTransferListener  {
+
+
+        @Override
+        public void started() {
+       //     Toast.makeText(getActivity()"Start Upload",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity(),"Strat Uploade",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void transferred(int i) {
+
+        }
+
+        @Override
+        public void completed() {
+
+        }
+
+        @Override
+        public void aborted() {
+
+        }
+
+        @Override
+        public void failed() {
+
+        }
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.menu_register,menu);
+        inflater.inflate(R.menu.menu_register, menu);
+
     }
 
     private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
-        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register ");
         ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
         ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -73,6 +224,7 @@ public class RegisterFragment extends Fragment {
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
+        setHasOptionsMenu(true);
     }
 
     @Override
